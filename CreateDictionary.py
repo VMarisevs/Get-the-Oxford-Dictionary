46bc6993bc93942b785ea47b3d74761f4f436415
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-02-17 00:58:48 -0800

    all links explored and map created

--- CreateDictionary.py
+++ CreateDictionary.py
@@ -9,10 +9,11 @@ import urllib
 from bs4 import BeautifulSoup
 
 link = "http://www.oxfordlearnersdictionaries.com/wordlist/english/oxford3000/"
-links = []
+links = { link : [link]}
 
 # Reading all urls with words
-#
+# This part is opens first page and populates the map with links
+
 page = urllib.urlopen(link).read()
  
 soup_page = BeautifulSoup(page, "html.parser").find('div', id='entries-selector')
@@ -20,7 +21,22 @@ soup_page = BeautifulSoup(page, "html.parser").find('div', id='entries-selector'
 tags_with_url = soup_page.find_all('a')
 
 for tag in tags_with_url:
-	links.append(tag['href'])
+	links[tag['href']] = [tag['href']]
 
-print(links)
-print(len(links))
+#
+# print(len(links))
+
+# now we are reading all pages for letter range
+# exploring all links 
+for link in links:	
+	page = urllib.urlopen(link).read()
+	soup_page = BeautifulSoup(page, "html.parser").find('div', id='paging')
+	tags_with_url = soup_page.find_all('a')
+	for tag in tags_with_url:
+		links[link].append(tag['href'])
+	
+# print(links)
+# running all links
+for link_array in links:
+	for link in links[link_array]:
+		print(link)
